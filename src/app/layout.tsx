bafe5f3936dd1cb26633de63e2a07d9a631f6ebb
Bernard Fiagbenu <bernardfiagbenu1@gmail.com>
2025-08-19 14:48:33 +0000

    the inintial page should  a dark background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
         <UserProvider>
             <ThemeProvider
                 attribute="class"
-                defaultTheme="light"
+                defaultTheme="dark"
                 enableSystem={false}
             >
             <Header />
@@ -114,4 +114,3 @@ export default function RootLayout({
     </html>
   );
 }
-
