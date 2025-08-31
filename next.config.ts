531f6ebc008b0074338eb7e01951f6cb3ce32094
Bernard Fiagbenu <bernardfiagbenu1@gmail.com>
2025-08-31 23:26:29 +0000

    Error: Invalid src prop (https://picsum.photos/600/400) on `next/image`,

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -10,6 +11,12 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'placehold.co',
